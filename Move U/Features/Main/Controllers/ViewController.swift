6846af0f250b6e21a9ae17d1c900d1e57febb67d
Jaime Casado <jaime.casado.a@gmail.com>
2019-05-21 14:23:30 +0200

    Root Commit

---
+++ Move U/Features/Main/Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Move U
+//
+//  Created by Jaime Casado Aparicio on 21/5/19.
+//  Copyright © 2019 Jaime Casado Aparicio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
